28933a8f7c2538649a0cbfdd7b85804614b9fbec
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2024-02-10 22:51:35 +0200

    added submit message form & styled the app a bit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        date: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        date: new Date(),
+    },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+    res.render("form", { title: "Mini message board" });
 });
 
 module.exports = router;
